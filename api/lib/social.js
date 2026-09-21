484b52222fe4472012d29e9549c3d170115b7d29
ZUKO-MD <132104247+Neggy5@users.noreply.github.com>
2026-09-21 19:15:50 +0100

    Add files via upload

--- api/lib/social.js
+++ api/lib/social.js
@@ -5,7 +5,8 @@ const SOCIAL_HOSTS = {
   instagram: [/^(?:www\.)?instagram\.com$/i],
   facebook: [/^(?:www\.)?facebook\.com$/i, /^(?:www\.)?fb\.watch$/i, /^(?:www\.)?fb\.com$/i],
   twitter: [/^(?:www\.)?(?:twitter|x)\.com$/i],
-  pinterest: [/^(?:www\.)?pinterest\.com$/i, /^(?:www\.)?pin\.it$/i]
+  pinterest: [/^(?:www\.)?pinterest\.com$/i, /^(?:www\.)?pin\.it$/i],
+  snapchat: [/^(?:www\.)?snapchat\.com$/i, /^(?:www\.)?t\.snapchat\.com$/i]
 };
 
 function detectPlatform(rawUrl) {
@@ -15,7 +16,7 @@ function detectPlatform(rawUrl) {
   for (const [platform, patterns] of Object.entries(SOCIAL_HOSTS)) {
     if (patterns.some(re => re.test(host))) return platform;
   }
-  throw new Error('Unsupported social platform. Supported: TikTok, Instagram, Facebook, X/Twitter and Pinterest.');
+  throw new Error('Unsupported social platform. Supported: TikTok, Instagram, Facebook, X/Twitter, Pinterest and Snapchat.');
 }
 
 module.exports = { detectPlatform, SOCIAL_HOSTS };
